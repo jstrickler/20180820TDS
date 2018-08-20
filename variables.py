2fb4600a7de6249ec5703738fea79fc4106f1174
jstrickler <jstrickler@gmail.com>
2018-08-20 12:56:26 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+x = 5
+
+customer_id = 'smi123.47x'  # using the short form
+
+print(customer_id)
+
+result = x * 57
+print(result)
+
+print(result + 1000)
+
+# print(y)
+
+#  a-z A-Z 0-9 _
+
+# class = 'freshman'
+#
+# yield  = 5
+
+city = 'Madison'
+
+number = 22
+
+# result = city + number
+#
+# print(result)
+
+print(type(city), type(number))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
