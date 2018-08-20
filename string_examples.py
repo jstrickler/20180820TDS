fbcdc6970a68f336de43e637133aca86270944ad
jstrickler <jstrickler@gmail.com>
2018-08-20 13:15:02 -0500

    string syntax

---
+++ string_examples.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+s1 = "spam\n"
+s2 = 'spam\n'
+
+print('It is a "good" thing')
+print("It's a good thing")
+
+print("""It's a "good" thing""")
+
+s3 = r'spam\n'
+print(s1)
+print(s2)
+print(s3)
+
